80ae55825a39572b0176d6c4ef08c068047c86dc
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-04-09 14:19:01 +0900

    Impl init command

---
+++ cmd/init.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init contest-name",
+	Short: "Initialize contest directory",
+	Long:  `Initialize contest directory`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New(`contest-name must be provided`)
+		}
+		directory := args[0]
+
+		settingsJsonContent := GetVscodeSettingsFileContent()
+
+		if err := os.Mkdir(directory, 0755); err != nil {
+			return err
+		}
+		if err := os.Mkdir(filepath.Join(directory, ".vscode"), 0755); err != nil {
+			return err
+		}
+		if err := os.WriteFile(filepath.Join(directory, ".vscode", "settings.json"), []byte(settingsJsonContent), 0644); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
