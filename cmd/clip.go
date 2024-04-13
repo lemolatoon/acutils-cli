64abd5e9cea337f2aab980beaecaaa86b2695360
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-04-13 17:28:20 +0900

    Clean up comments

--- cmd/clip.go
+++ cmd/clip.go
@@ -78,14 +78,4 @@ Please copy the source code manually.
 
 func init() {
 	rootCmd.AddCommand(clipCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// clipCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// clipCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
